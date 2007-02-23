7698db9b6890bfc125ade46e4152293bd36426de
mshatny <mshatny@f96168c2-964d-0410-8f50-4d3b87a14ac3>
2007-02-23 15:55:16 +0000

    [ENH]  Enhancements to GridViewSearchPanel. More Info: http://community.codesmithtools.com/forums/thread/22549.aspx [FIX]  Fixing problem in ComponentServiceBase.cst with hard returns for XML comments (contrib Taliesin)

--- source/trunk/Source/TemplateLib/CreateTemplates.cs
+++ source/trunk/Source/TemplateLib/CreateTemplates.cs
@@ -191,6 +191,7 @@
 			CodeTemplates.Add("BoundEntityDropDownField.cs.cst", base.CreateTemplate<BoundEntityDropDownField>()); this.PerformStep();
 			CodeTemplates.Add("BoundRadioButtonField.cs.cst", base.CreateTemplate<BoundRadioButtonField>()); this.PerformStep();			
 			CodeTemplates.Add("GridViewSearchPanel.cs.cst", base.CreateTemplate<GridViewSearchPanel>()); this.PerformStep();
+			CodeTemplates.Add("GridViewSearchPanelState.cs.cst", base.CreateTemplate<GridViewSearchPanelState>()); this.PerformStep();
 																	
 			CodeTemplates.Add("TableRepeater.cst", base.CreateTemplate<TableRepeater>()); this.PerformStep();
 			CodeTemplates.Add("ViewRepeater.cst", base.CreateTemplate<ViewRepeater>()); this.PerformStep();
